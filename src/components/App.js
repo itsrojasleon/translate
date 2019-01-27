54de58ce206f30b63f5c73c8729ec31145cd8e12
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-01-27 15:53:38 -0600

    Consuming the context value

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,25 @@ import React from 'react';
 import UserCreate from './UserCreate';
 
 class App extends React.Component {
-  state = { language: 'english' }
+  state = { language: 'english' };
 
   onLanguageChange = language => {
     this.setState({ language });
-  }
+  };
 
   render() {
     return (
       <div className="ui container">
         <div>
           Select a language
-          <i className="flag us" onClick={() => this.onLanguageChange('english')} />
-          <i className="flag nl" onClick={() => this.onLanguageChange('dutch')} />
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange('english')}
+          />
+          <i
+            className="flag nl"
+            onClick={() => this.onLanguageChange('dutch')}
+          />
         </div>
         <UserCreate />
       </div>
