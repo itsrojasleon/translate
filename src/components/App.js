98bf558f2f4facbff678aa30ee4d2d26303c7d12
Juan Luis Rojas León <rojasleon122@gmail.com>
2019-01-28 13:50:16 -0600

    Rendering the language store

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import UserCreate from './UserCreate';
 import LanguageSelector from './LanguageSelector';
 // Contexts
-import LanguageContext from '../contexts/LanguageContext';
+import { LanguageStore } from '../contexts/LanguageContext';
 import ColorContext from '../contexts/ColorContext';
 
 class App extends React.Component {
@@ -16,13 +16,13 @@ class App extends React.Component {
   render() {
     return (
       <div className="ui container">
-        <LanguageSelector onLanguageChange={this.onLanguageChange} />
-        {/* Just for fun */}
-        <ColorContext.Provider value="red">
-          <LanguageContext.Provider value={this.state.language}>
+        <LanguageStore>
+          <LanguageSelector onLanguageChange={this.onLanguageChange} />
+
+          <ColorContext.Provider value="red">
             <UserCreate />
-          </LanguageContext.Provider>
-        </ColorContext.Provider>
+          </ColorContext.Provider>
+        </LanguageStore>
       </div>
     );
   }
--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Context = React.createContext('english');
 
-class LanguageStore extends React.Component {
+export class LanguageStore extends React.Component {
   state = { language: 'english' };
 
   onLanguageChange = language => {
@@ -10,7 +10,8 @@ class LanguageStore extends React.Component {
   };
   render() {
     return (
-      <Context.Provider value={{ ...this.state, onLanguageChange }}>
+      <Context.Provider
+        value={{ ...this.state, onLanguageChange: this.onLanguageChange }}>
         {this.props.children}
       </Context.Provider>
     );
